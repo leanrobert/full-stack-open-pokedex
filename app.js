f3720ec07823406d2753650e5028a3cda79d3274
Leandro Robert <leanrobert24@gmail.com>
2023-04-11 09:52:42 -0300

    Exercise 11.11 Health check and rollback

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static("dist"));
 
 app.get('/health', (req, res) => {
-  throw 'Error...'
-  //eslint-disable-next-line
   res.send('ok')
 })
 

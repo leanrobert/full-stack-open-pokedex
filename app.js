c7e8b65cae58b9c276c23cd12a6505271ad3777f
Leandro Robert <leanrobert24@gmail.com>
2023-04-11 09:44:10 -0300

    Should fail deploy pipeline

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static("dist"));
 
 app.get('/health', (req, res) => {
+  throw 'Error...'
+  //eslint-disable-next-line
   res.send('ok')
 })
 
